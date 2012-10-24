3cd3b20cddcef078ffab9cf9590b1e1803e61c21
Marvin Baptista <marvinbaptista@gmail.com>
2012-10-24 15:13:18 -0430

    Primera Carga
    Creacion de Repositorio y archivos de Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usclinica::Application.config.secret_token = '422735351badb30703bf3cab9db6c29cc9213a754b2add9df8c4fd188caaf15840e9234775b68228a1b675b177a1077de9021bc785a7fd214ec6ff5f58061f8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Usclinica::Application.config.session_store :cookie_store, key: '_usclinica_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Usclinica::Application.config.session_store :active_record_store
